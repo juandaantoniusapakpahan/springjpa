3bc8a59dce45015c318eac71f1f61e2126bac9aa
juandaantoniuspakpahan <juanda.pakpahan@phincon.com>
2025-12-15 14:05:23 +0700

    item

--- src/main/java/com/backend/springjpa/controller/CartController.java
+++ src/main/java/com/backend/springjpa/controller/CartController.java
@@ -2,13 +2,17 @@ package com.backend.springjpa.controller;
 
 import com.backend.springjpa.dto.ApiResponse;
 import com.backend.springjpa.dto.CartDto;
+import com.backend.springjpa.dto.ProductDto;
 import com.backend.springjpa.service.CartService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -20,9 +24,21 @@ public class CartController {
         this.cartService = cartService;
     }
 
-    @PostMapping("/upsertCart/item")
-    public ResponseEntity<ApiResponse<String>> upsertCartItem(@Valid @RequestBody CartDto cartDto) {
-        cartService.upsertCartItem(cartDto);
-        return ResponseEntity.ok(ApiResponse.ok("", "Cart item added successfully", "/upsertCart/item"));
+    @PostMapping("/addCart/item")
+    public ResponseEntity<ApiResponse<String>> upsertCartItem(@Valid @RequestBody CartDto dto) {
+        cartService.addCartItem(dto);
+        return ResponseEntity.ok(ApiResponse.ok( "Cart item added successfully",null, "/addCart/item"));
+    }
+
+    @PostMapping("/reduceCart/item")
+    public ResponseEntity<ApiResponse<String>> reduceCartItem(@Valid @RequestBody CartDto dto) {
+        cartService.reduceCartItem(dto);
+        return ResponseEntity.ok(ApiResponse.ok("Cart item reduced successfully", null, "/reduceCart/item"));
+    }
+
+    @PostMapping("/getCartGroupedByCategory/{userId}")
+    public ResponseEntity<ApiResponse<List<ProductDto>>> getCartGroupedByCategory(@PathVariable Long userId) {
+        List<ProductDto> dto = cartService.getCartGroupedByCategory(userId);
+        return ResponseEntity.ok(ApiResponse.ok("Success",dto, "/getCartGroupedByCategory"));
     }
 }
--- src/main/java/com/backend/springjpa/service/CartService.java
+++ src/main/java/com/backend/springjpa/service/CartService.java
@@ -2,17 +2,27 @@ package com.backend.springjpa.service;
 
 
 import com.backend.springjpa.dto.CartDto;
+import com.backend.springjpa.dto.ProductDto;
+import com.backend.springjpa.dto.ProductVariantDto;
 import com.backend.springjpa.entity.Cart;
 import com.backend.springjpa.entity.CartItem;
+import com.backend.springjpa.entity.Product;
 import com.backend.springjpa.entity.ProductVariant;
 import com.backend.springjpa.exception.BadRequestException;
+import com.backend.springjpa.exception.ResourceNotFoundException;
+import com.backend.springjpa.mapper.ProductMapper;
+import com.backend.springjpa.mapper.ProductVariantMapper;
 import com.backend.springjpa.repository.CartItemRepository;
 import com.backend.springjpa.repository.CartRepository;
 import com.backend.springjpa.repository.ProductVariantRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CartService {
@@ -29,31 +39,63 @@ public class CartService {
     }
 
     @Transactional
-    public void upsertCartItem(CartDto cartDto) {
+    public void addCartItem(CartDto cartDto) {
         Cart cart = cartRepository.findByUserId(Long.parseLong(cartDto.getUserId()))
                 .orElseGet(() -> {
                     Cart newCart = new Cart();
                     newCart.setUserId(Long.parseLong(cartDto.getUserId()));
-                    return newCart;
+                    return cartRepository.save(newCart);
                 });
-
         ProductVariant productVariant = productVariantRepository.findById(Long.parseLong(cartDto.getProductVariantId()))
                 .orElseThrow(() -> new RuntimeException("Product variant not found"));
 
-        if (productVariant.getStockQty() < Integer.parseInt(cartDto.getQuantity())) {
+        CartItem existingItem = cartItemRepository.findByCartIdAndProductVariantId(cart.getId(), productVariant.getId())
+                .orElseGet(()->{
+                    CartItem newCartItem = new CartItem();
+                    newCartItem.setCart(cart);
+                    newCartItem.setProductVariant(productVariant);
+                    newCartItem.setQuantity(0);
+                    return newCartItem;
+                });
+        existingItem.setQuantity(existingItem.getQuantity() + Integer.parseInt(cartDto.getQuantity()));
+
+        if (existingItem.getQuantity() > productVariant.getStockQty()) {
             throw new BadRequestException("Not enough stock");
         }
+        cartItemRepository.save(existingItem);
+    }
 
-        Optional<CartItem> existingItem = cartItemRepository.findByCartIdAndProductVariantId(cart.getId(), productVariant.getId());
-        if (existingItem.isPresent()) {
-            existingItem.get().setQuantity(
-                    existingItem.get().getQuantity() + Integer.parseInt(cartDto.getQuantity()));
+    @Transactional
+    public void reduceCartItem(CartDto dto) {
+        Cart cart = cartRepository.findByUserId(Long.parseLong(dto.getUserId()))
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+
+        CartItem item =cartItemRepository.findByCartIdAndProductVariantId(cart.getId(), Long.parseLong(dto.getProductVariantId()))
+                .orElseThrow(() -> new ResourceNotFoundException("Item not found"));
+
+        Integer reducedAmount = Integer.parseInt(dto.getQuantity());
+        if (item.getQuantity()-reducedAmount <= 0) {
+            cartItemRepository.delete(item);
         } else {
-            CartItem newCartItem = new CartItem();
-            newCartItem.setCart(cart);
-            newCartItem.setProductVariant(productVariant);
-            newCartItem.setQuantity(Integer.parseInt(cartDto.getQuantity()));
-            cartItemRepository.save(newCartItem);
+            item.setQuantity(item.getQuantity() - reducedAmount);
+            cartItemRepository.save(item);
         }
+    }
+
+    public List<ProductDto> getCartGroupedByCategory(Long userId) {
+        Cart cart = cartRepository.findByUserId(userId).orElseThrow(()-> new ResourceNotFoundException("Cart not found"));
+        List<CartItem> cartItems = cartItemRepository.findByCartId(cart.getId());
+
+        Map<Long, ProductDto> map = new LinkedHashMap<>();
+
+        for (CartItem ci: cartItems) {
+            Product product = ci.getProductVariant().getProduct();
+            ProductDto productDto = map.computeIfAbsent(
+                    product.getId(),
+                    id-> ProductMapper.toProductDTO(product));
+            ProductVariantDto productVariantDto = ProductVariantMapper.toProductVariantDto(ci.getProductVariant());
+            productDto.getVariants().add(productVariantDto);
+        }
+        return new ArrayList<>(map.values());
     }
 }
